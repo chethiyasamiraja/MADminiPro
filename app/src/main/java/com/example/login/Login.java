7bff71ee2e4fadeebfa5b07e4beece8f9b5903cf
ANURADHA <chethiyasamiraja@gmail.com>
2020-10-04 04:29:03 -0700

    MADminiPro4

--- app/src/main/java/com/example/login/Login.java
+++ app/src/main/java/com/example/login/Login.java
@@ -12,6 +12,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -23,6 +26,9 @@ public class Login extends AppCompatActivity {
     DatabaseReference ref;
     Add add;
 
+    AwesomeValidation awesomeValidation;
+
+
     private void clearcontrols() {
 
         txt1.setText("");
@@ -33,6 +39,14 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        awesomeValidation.addValidation(this,R.id.txt2,
+                "[0-9]{6}$",R.string.invalid_password);
+        awesomeValidation.addValidation(this,R.id.txt1,
+                RegexTemplate.NOT_EMPTY,R.string.invalid_name);
 
         txt1 = findViewById(R.id.txt1);
         txt2 = findViewById(R.id.txt2);
@@ -64,7 +78,7 @@ public class Login extends AppCompatActivity {
             public void onClick(View view) {
                 ref = FirebaseDatabase.getInstance().getReference().child("add");
 
-                try {
+               /* try {
 
                     if (TextUtils.isEmpty(txt1.getText().toString()))
                         Toast.makeText(getApplicationContext(), "First Name is required", Toast.LENGTH_LONG).show();
@@ -87,9 +101,31 @@ public class Login extends AppCompatActivity {
                     }
                 } finally {
 
-                };
+                };*/
+
+
+
+                if(awesomeValidation.validate()){
+                    add.setName(txt1.getText().toString().trim());
+                    add.setPass(txt2.getText().toString().trim());
+
+                    // ref.push().setValue(add);
+                    ref.child("Login").setValue(add);
+
+                    Toast.makeText(getApplicationContext(), "Data inserted successfully", Toast.LENGTH_LONG).show();
+                    clearcontrols();
+
+                    Intent i = new Intent(getApplicationContext(), Profile.class);
+                    startActivity(i);
+
+                }else {
+                    Toast.makeText(getApplicationContext(),"Validation Failed..",Toast.LENGTH_SHORT).show();
+                }
+
+
             }
         });
+
 
 
     }
